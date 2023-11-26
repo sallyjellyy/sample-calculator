648021f3c80c6379e0d54940d47c6ca99ceb7602
sallyjellyy <min27604@gmail.com>
2023-11-26 16:17:54 +0900

    init

---
+++ src/Calculator.tsx
@@ -0,0 +1,25 @@
+export const Calculator = () => {
+    return     <>
+    <div style={{display:"flex"}}>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>+</div>
+    </div>
+    <div style={{display:"flex"}}>
+        <div>6</div>
+        <div></div>
+        <div>9</div>
+        <div>+</div></div>
+    <div style={{display:"flex"}}>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>+</div></div>
+    <div style={{display:"flex"}}>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>+</div></div>
+    </>
+}
